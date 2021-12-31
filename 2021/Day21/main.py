fc7c3c3b7ec85b0fd12cb565928b5f4bcda7b868
Joseph Boeckman <altoidbox@gmail.com>
2021-12-31 09:50:39 -0500

    Complete solution to 2021 Day21

--- 2021/Day21/main.py
+++ 2021/Day21/main.py
@@ -45,51 +45,77 @@ def part1(*args):
      DiracDiceGame(DeterministicDice(), *args).play()
 
 
+# All possible outcomes of rolling a 3-sided die 3 times
+#  key: Sum of the roll values
+#  value: Number of instances of this possible outcome
 POSSIBILITIES = Counter([sum(x) for x in itertools.product(list(range(1, 4)), repeat=3)])
 
+# Maps a tuple (position, score) to all possible outcomes from that position
+# Outcomes are stored as a mapping. 
+#  key: Number of turns to reach a winning score
+#  value: Number of instances of this possibility
 ScoreMap = {}
 WINNING_COUNTER = Counter({0: 1})
 
 
 def sim1(pos, score):
     if score >= 21:
+        # Base case - Once the score has reached 21, the game is over. 0 turns, 1 possibility
         return WINNING_COUNTER
     key = (pos, score)
+    # The answer never changes for a given position, just remember and return it
     answer = ScoreMap.get(key, None)
     if answer is not None:
         return answer
     answer = Counter()
+    # For each possible roll
     for roll, roll_count in POSSIBILITIES.items():
-        new_pos = (pos + roll) % 10
-        new_score = score + new_pos + 1
+        # calculate a new position
+        new_pos = (pos + roll - 1) % 10 + 1
+        # and a new score
+        new_score = score + new_pos
+        # For each possible outcome at that position and score
         for win_turns, win_count in sim1(new_pos, new_score).items():
-            answer[win_turns+1] += (win_count * roll_count)
+            # calculate the number of instances we can get it from here (*roll_count)
+            # and increase the number of turns taken to win by 1
+            answer[win_turns + 1] += (win_count * roll_count)
     ScoreMap[key] = answer
     return answer
 
 
+def calc_player_possibilities(start_pos):
+    turns_to_win = sim1(start_pos, 0)
+    # Number of universes that make it through each round. In the 0th round, there is only 1 universe
+    universes_in_turn = [1]
+    turns = len(universes_in_turn)
+    while universes_in_turn[-1] != 0:
+        # number of universes in the previous turn, times the number of new universes created
+        # by rolling the dice 3 times (3**3). Subtract out the number of universes that win this turn.
+        universes_in_turn.append(universes_in_turn[turns - 1] * (3**3) - turns_to_win[turns])
+        turns += 1
+
+    print(sorted(turns_to_win.items()), sum(turns_to_win.values()))
+    print(universes_in_turn)
+
+    return turns_to_win, universes_in_turn
+
+
 def part2(p1, p2):
-    for score in range(21):
-        for pos in range(10):
-            sim1(pos, score)
-    p1_rounds = sim1(p1-1, 0)
-    p2_rounds = sim1(p2-1, 0)
-    print(sorted(p1_rounds.items()), sum(p1_rounds.values()))
-    print(sorted(p2_rounds.items()), sum(p2_rounds.values()))
+    p1_rounds, poss1 = calc_player_possibilities(p1)
+    p2_rounds, poss2 = calc_player_possibilities(p2)
+    
     p1_wins = 0
     p2_wins = 0
-    poss1 = [27**0, 27**1, 27**2]
-    poss2 = [27**0, 27**1, 27**2]
-    for turns1, wins1 in sorted(p1_rounds.items()):
-        poss1.append(poss1[turns1-1] * 27 - wins1)
-        #p1_wins += wins1 * (3**(2*turns1-1))
-        #for turns2, wins2 in p2_rounds.items():
-        #    if turns2 < turns1:
-        #        p1_wins -= wins2 * (3**(2*turns2))
-    for turns2, wins2 in sorted(p2_rounds.items()):
-        poss2.append(poss2[turns2-1] * 27 - wins2)
-    print(poss1)
-    print(poss2)
+
+    for turns, wins in sorted(p1_rounds.items()):
+        # since p1 goes first, it defeats all opponent universes that make it through the previous round
+        p1_wins += wins * poss2[turns-1]
+    for turns, wins in sorted(p2_rounds.items()):
+        # p2 defeats all opponent universes that make it through the same number of rounds
+        p2_wins += wins * poss1[turns]
+    
+    print('p1:', p1_wins)
+    print('p2:', p2_wins)
 
 
 if __name__ == "__main__":
@@ -98,3 +124,4 @@ if __name__ == "__main__":
     #part2(sys.argv[1])
     print(POSSIBILITIES)
     part2(4, 8)
+    part2(6, 2)
